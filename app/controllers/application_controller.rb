bc3b0ad3d6512ae7ddeca6d1f6edb4d028373bd9
Billy Irwin <birwin93@gmail.com>
2013-07-28 18:02:18 -0400

    added pg gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :save_location
+
+  private
+    def save_location
+      session[:back] = session[:tmp_url] || root_url
+      session[:tmp_url] = request.url
+    end
+
 end
